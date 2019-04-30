3db3de6ff095cd9c8c2fcdc5680153432917799c
Vivek <vshrm1994@gmail.com>
2019-04-30 12:10:59 +0530

    chit widget bug fixed

--- lib/widget/chats.dart
+++ lib/widget/chats.dart
@@ -2,42 +2,47 @@ import 'package:flutter/material.dart';
 import 'package:facebook_messenge_ui/utils/string.dart';
 
 class Chats extends StatelessWidget {
-  var data = <Widget>[
-    chats("Deshmukh", "Hi, how are you?", profileUrl, true),
+  final data = <Widget>[
+    chats("Deshmukh", "Hi, how are you?", profileUrl, true, 0),
     chats("Vijay", "Say hi to new Facebook friend friend friend",
-        userProfileUrl1, false),
-    chats("Akhilesh", "Hi, how are you?", userProfileUrl2, true)
+        userProfileUrl1, false, 1),
+    chats("Akhilesh", "Hi, how are you?", userProfileUrl2, true, 2),
+    chats("Dhananjay", "Hi, how are you?", userProfileUrl1, false, 0)
   ];
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      scrollDirection: Axis.vertical,
-      child: Column(
-        children: data,
-      ),
+    return ListView(
+      shrinkWrap: true,
+      children: data,
     );
   }
 
-  static Widget chats(String name, String msg, String imgUrl, bool readStatus) {
-    return Container(
-      margin: EdgeInsets.only(top: 16),
+  static Widget chats(
+      String name, String msg, String imgUrl, bool readStatus, int status) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 16),
       child: Row(
         children: <Widget>[
-          Container(
-            width: 55,
-            height: 55,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              image: DecorationImage(
-                image: NetworkImage(imgUrl),
-                fit: BoxFit.fill,
-              ),
-            ),
+          status == 0
+              ? Container(
+                  width: 55,
+                  height: 55,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                      image: NetworkImage(imgUrl),
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                )
+              : status == 1 ? storyOnline(imgUrl) : storyOffline(),
+          SizedBox(
+            width: 10,
           ),
-          Padding(
-            padding: const EdgeInsets.only(left: 10),
+          Expanded(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
               children: <Widget>[
                 Text(
                   name,
@@ -47,23 +52,30 @@ class Chats extends StatelessWidget {
                     fontFamily: 'Medium',
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 6),
-                  child: Row(
-                    children: <Widget>[
-                      Text(
+                SizedBox(
+                  height: 6,
+                ),
+                Row(
+                  mainAxisSize: MainAxisSize.max,
+                  children: <Widget>[
+                    Flexible(
+                      child: Text(
                         msg,
                         overflow: TextOverflow.ellipsis,
-                        maxLines: 1,
-                        softWrap: true,
                         style: TextStyle(
-                          color: readStatus ? Colors.grey[400] : Colors.black,
-                          fontSize: 16,
-                          fontFamily: 'Regular',
+                          color: Colors.grey[400],
+                          fontSize: 14,
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                    Text(
+                      " 30 Apr",
+                      style: TextStyle(
+                        color: Colors.grey[400],
+                        fontSize: 14,
+                      ),
+                    )
+                  ],
                 )
               ],
             ),
@@ -72,4 +84,72 @@ class Chats extends StatelessWidget {
       ),
     );
   }
+}
+
+Widget storyOnline(String img) {
+  return Stack(
+    children: <Widget>[
+      Container(
+        width: 55,
+        height: 55,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          image: DecorationImage(
+            image: NetworkImage(
+              img,
+            ),
+            fit: BoxFit.fill,
+          ),
+        ),
+      ),
+      Positioned(
+        bottom: 0,
+        right: 0,
+        child: Container(
+          width: 20,
+          height: 20,
+          decoration: BoxDecoration(
+            color: Colors.transparent,
+            shape: BoxShape.circle,
+            border: Border.all(color: Colors.white, width: 3),
+          ),
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.green,
+              shape: BoxShape.circle,
+            ),
+          ),
+        ),
+      )
+    ],
+  );
+}
+
+Widget storyOffline() {
+  return Container(
+    width: 55,
+    height: 55,
+    padding: EdgeInsets.all(3),
+    decoration: BoxDecoration(
+      shape: BoxShape.circle,
+      color: Colors.white,
+      border: Border.all(
+        color: Colors.blue,
+        width: 3,
+      ),
+    ),
+    child: Container(
+      width: 50,
+      height: 50,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        image: DecorationImage(
+          image: NetworkImage(
+            profileUrl,
+          ),
+          fit: BoxFit.fill,
+        ),
+      ),
+    ),
+  );
 }
